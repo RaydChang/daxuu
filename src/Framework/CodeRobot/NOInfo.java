14e7e3db5e83a6684697b76a5d7295999da85f82
lucky <lucky@localhost>
2013-05-21 11:56:29 +0800

    0521

---
+++ src/Framework/CodeRobot/NOInfo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Framework.CodeRobot;
+
+/**
+ * @author USER
+ *
+ */
+public class NOInfo {
+
+	/**
+	 * 
+	 */
+	public NOInfo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
